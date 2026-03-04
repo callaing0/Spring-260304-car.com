a0afc041c33da67cdf82deb1dc06f85b9e9ed870
callaing0 <0hmong0212@gmail.com>
2026-03-04 12:25:20 +0900

    java/com.book/control

--- CarController.java
+++ CarController.java
@@ -1,19 +1,37 @@
 package com.book.control;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.book.Entity.CarMemberDTO; 
+import com.book.Repository.CarMemberRepository;
 
 @Controller
 public class CarController {
 	
-	@GetMapping("/agree")
-	public String showPrivacy() {
-	    return "car/agree"; // 개인정보 동의 페이지 호출
-	}
+    @Autowired
+    private CarMemberRepository carmemberRepository;
+
+    // 회원가입 처리 (signup.html의 action과 맞춤)
+    @PostMapping("/signup") 
+    public String register(CarMemberDTO member) { // Member 대신 CarMemberDTO 사용
+        // 1. DB에 회원 정보 저장 (오타 수정: caemember -> carmember)
+        carmemberRepository.save(member);
+        
+        // 2. 가입 완료 후 로그인 페이지로 이동
+        return "redirect:/login";
+    }
+    
+    @GetMapping("/agree")
+    public String showPrivacy() {
+        return "car/agree"; 
+    }
 
     @GetMapping("/")
     public String home() {
-    	return "car/index"; 
+        return "car/index"; 
     }
 
     @GetMapping("/login")
@@ -23,6 +41,6 @@ public class CarController {
 
     @GetMapping("/signup")
     public String showSignupForm() {
-        return "car/signup"; // 실제 회원가입 양식 페이지 호출
+        return "car/signup"; 
     }
 }
