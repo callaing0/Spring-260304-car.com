63a47f7a96d89032d9453cb61d224a31c5749918
callaing0 <0hmong0212@gmail.com>
2026-03-04 15:26:12 +0900

    java/com.book/Repository

--- CarMemberRepository.java
+++ CarMemberRepository.java
@@ -1,10 +1,14 @@
 package com.book.Repository;
 
-import com.book.Entity.CarMemberDTO; // Member 대신 CarMemberDTO 임포트
+import com.book.Entity.CarMemberDTO; 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CarMemberRepository extends JpaRepository<CarMemberDTO, Long> {
-    // 인터페이스(interface)로 선언해야 스프링 JPA가 자동으로 구현체를 만들어줍니다.
+    
+    // 이메일로 회원 정보를 조회하는 기능을 추가합니다.
+    // 메서드 이름 규칙에 따라 Spring Data JPA가 자동으로 SQL(SELECT * FROM carmember WHERE email = ?)을 생성합니다.
+    CarMemberDTO findByEmail(String email);
+    
 }
